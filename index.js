ea77d282d258e96c4a4bac5842d25adaf201942b
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-07-18 13:00:40 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+// saturdayFun function declaration as specified
+// 1) function exists
+// 2) uses the default activity 'roller-skate' when no arguments are passed
+// 3) permits the default activity to be overriden
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// mondayWork function expression as specified
+// 1) function exists
+// 2) uses the default activity 'go to the office' when no arguments are passed
+// 3) permits the default activity to be overriden
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// wrapAdjective function according to the specification
+// 1) function exists
+// 2) when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
+// 3) when initialized with '||' creates a function that, when called, wraps an adjective in a highlight
+function wrapAdjective(wrapper) {
+  return function (adjective) {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
